61fd2d1dbc8954ca95053248ffdec4396e1b7ea8
Alexis JC <alexisjc.dev@gmail.com>
2022-02-04 10:09:45 +0100

    feat: first draft * Display * Position * Backdrop

--- src/wwSection.vue
+++ src/wwSection.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="my-section">
-    <h1 :style="textStyle">My Title</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
-    </p>
+  <div 
+    class="modal-container" 
+    :style="backdropStyle"
+  >
+    <transition name="fade" mode="out-in">
+      <wwLayout 
+        v-if="show" 
+        class="modal-dropzone" 
+        direction="column" 
+        path="modalContent" 
+        :class="content.position" 
+        :style="modalContentStyle"
+      ></wwLayout>
+    </transition>
   </div>
 </template>
 
@@ -14,28 +20,124 @@
 export default {
   props: {
     content: { type: Object, required: true },
+    /* wwEditor:start */
+    wwEditorState : { type: Object, required: true }
+    /* wwEditor:end */
+  },
+  data() {
+    return {
+      wwDefaultContent: {
+        modalContent: []
+      }
+    }
   },
   computed: {
-    textStyle() {
+    show () {
+      /* wwEditor:start */
+      return this.content.display || this.wwEditorState.sidepanelContent.displayInEditor;
+      /* wwEditor:end */
+      return this.content.display;
+    },
+    backdropStyle() {
       return {
-        color: this.content.textColor,
+        backgroundColor: (this.show && this.content.backdrop) ? this.content.backdropColor : 'transparent',
+        pointerEvents: (this.show && this.content.backdrop) ? 'auto' : 'none'
       };
     },
-  },
+    modalContentStyle() {
+      if (this.content.position !== 'custom') {
+        return {}
+      }
+      
+      return {
+          top: this.content.positionTop,
+          left: this.content.positionLeft,
+        }
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.my-section {
+.modal-container {
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  min-height: 200px;
-  h1 {
-    font-size: 32px;
-  }
-  p {
-    margin-top: 12px;
-  }
+  isolation: isolate;
+  position: fixed;
+  top: 0;
+  height: 100vh;
+  z-index: 30;
+  transition: background-color 0.5s;
 }
+
+.modal-dropzone {
+  position: absolute;
+  min-height: 100px;
+  min-width: 100px;
+  height: inset;
+  width: inset;
+}
+
+// .my-section {
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   min-height: 200px;
+//   h1 {
+//     font-size: 32px;
+//   }
+//   p {
+//     margin-top: 12px;
+//   }
+// }
+
+.topLeft {
+  top: 0;
+  left: 0;
+}
+.topMiddle {
+  top: 0;
+  left: 50%;
+  transform: translate(-50%);
+}
+.topRight {
+  top: 0;
+  right: 0;
+}
+.middleLeft {
+  top: 50%;
+  left: 0;
+  transform: translateY(-50%);
+}
+.middle {
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+}
+.middleRight {
+  top: 50%;
+  right: 0;
+  transform: translateY(-50%);
+}
+.bottomLeft {
+  bottom: 0;
+  left: 0;
+}
+.bottomMiddle {
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%);
+}
+.bottomRight {
+  bottom: 0;
+  right: 0;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+
 </style>
